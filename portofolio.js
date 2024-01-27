5e421c4965342a0a4117895ad349bf97aaa2f790
manzidavidrw <manzi2020d@gmail.com>
2024-01-27 22:07:51 +0200

    portfolio

---
+++ portofolio.js
@@ -0,0 +1,9 @@
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const hamburger = document.querySelector('.hamburger');
+    const navList = document.querySelector('.nav-list');
+
+    hamburger.addEventListener('click', function () {
+      navList.classList.toggle('show');
+    });
+  });
