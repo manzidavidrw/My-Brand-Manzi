c0bb30555796fd2fc6a2695dc7fe72295769653b
manzidavidrw <manzi2020d@gmail.com>
2024-02-04 22:52:49 +0200

    allign fields of creating blog

--- javascript/create.js
+++ javascript/create.js
@@ -1,7 +1,7 @@
 tinymce.init({
   selector: 'textarea#default',
-  width: 1000,
-  height: 300,
+  width: 810,
+  height: 250,
   plugins:[
       'advlist', 'autolink', 'link', 'image', 'lists', 'charmap', 'prewiew', 'anchor', 'pagebreak',
       'searchreplace', 'wordcount', 'visualblocks', 'code', 'fullscreen', 'insertdatetime', 'media', 
@@ -14,5 +14,41 @@ tinymce.init({
       favs: {title: 'menu', items: 'code visualaid | searchreplace | emoticons'}
   },
   menubar: 'favs file edit view insert format tools table',
-  content_style: 'body{font-family:Helvetica,Arial,sans-serif; font-size:16px}'
+  content_style: 'body{font-family:Helvetica,Arial,sans-serif; font-size:16px; }'
 });
+const title = document.getElementById('title');
+const upload = document.getElementById('upload');
+const text_editor = document.getElementById('default');
+const text_error = document.getElementById('text_error');
+const form =document.getElementById('form');
+const upload_error = document.getElementById('upload_error');
+const title_error = document.getElementById('title_error');
+
+
+
+form.addEventListener('submit', (e)=>{
+if(title.value === "" || title.value == null){
+    
+  title_error.innerHTML="**please enter Title**";
+}
+else{
+  e.preventDefault()
+  title_error.innerHTML="";
+}
+if(upload.value === "" || upload.value == null){
+    
+  upload_error.innerHTML="**please upload photo for a blog**";
+}
+else{
+  e.preventDefault()
+  upload_error.innerHTML="";
+}
+if(text_editor.value === "" || text_editor.value == null){
+    
+  text_error.innerHTML="**please enter Title**";
+}
+else{
+  e.preventDefault()
+  text_error.innerHTML="";
+}
+})
