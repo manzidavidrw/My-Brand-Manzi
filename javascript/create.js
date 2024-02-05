c4b3600b89e4494fed566c3b310a7bf4cc101182
manzidavidrw <manzi2020d@gmail.com>
2024-02-05 09:04:39 +0200

    allign fields of creating blog

--- javascript/create.js
+++ javascript/create.js
@@ -16,39 +16,3 @@ tinymce.init({
   menubar: 'favs file edit view insert format tools table',
   content_style: 'body{font-family:Helvetica,Arial,sans-serif; font-size:16px; }'
 });
-const title = document.getElementById('title');
-const upload = document.getElementById('upload');
-const text_editor = document.getElementById('default');
-const text_error = document.getElementById('text_error');
-const form =document.getElementById('form');
-const upload_error = document.getElementById('upload_error');
-const title_error = document.getElementById('title_error');
-
-
-
-form.addEventListener('submit', (e)=>{
-if(title.value === "" || title.value == null){
-    
-  title_error.innerHTML="**please enter Title**";
-}
-else{
-  e.preventDefault()
-  title_error.innerHTML="";
-}
-if(upload.value === "" || upload.value == null){
-    
-  upload_error.innerHTML="**please upload photo for a blog**";
-}
-else{
-  e.preventDefault()
-  upload_error.innerHTML="";
-}
-if(text_editor.value === "" || text_editor.value == null){
-    
-  text_error.innerHTML="**please enter Title**";
-}
-else{
-  e.preventDefault()
-  text_error.innerHTML="";
-}
-})
--- javascript/portofolio.js
+++ javascript/portofolio.js
@@ -9,11 +9,17 @@
   // });
   const hamburger = document.querySelector('.hamburger');
   const navList = document.querySelector('.nav-list');
+  const close_menu = document.querySelector('.close_menu')
 
   hamburger.addEventListener('click', show)
+  close_menu.addEventListener('click', close)
+
   function show(){
     navList.style.display= 'flex';
-    navList.style.top= '0';
+    navList.style.top= '-15px';
+  }
+  function close(){
+    navList.style.top= '-100%';
   }
 
 
