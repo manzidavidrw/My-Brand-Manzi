1f39bd9fc649224d8744a905375ed773ffbe8a8a
manzidavidrw <manzi2020d@gmail.com>
2024-02-05 10:26:42 +0200

    dashboard hamburger working

--- javascript/create1.js
+++ javascript/create1.js
@@ -38,10 +38,16 @@ else{
 
 const hamburger = document.querySelector('.hamburger');
 const navList = document.querySelector('.nav-list');
+const close_menu = document.querySelector('.close_menu')
 
 hamburger.addEventListener('click', show)
+close_menu.addEventListener('click', close)
+
 function show(){
   navList.style.display= 'flex';
   navList.style.left= '0';
-
 }
+function close(){
+  navList.style.top= '-100%';
+}
+
